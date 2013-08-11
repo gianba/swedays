3d44c24b167178012cf98d64f315290201687e56
gianba <gmbaschera@gmail.com>
2013-08-11 03:25:49 -0400

    Driver test added

---
+++ src/test/java/reuters/importer/FileSplitterDriverTest.java
@@ -0,0 +1,34 @@
+package reuters.importer;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Test;
+
+public class FileSplitterDriverTest {
+
+	@Test
+	public void testDriver() throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.default.name", "file:///");
+		conf.set("mapred.job.tracker", "local");
+		
+		Path input = new Path("Data");
+		Path output = new Path("splitted");
+		FileSystem fs = FileSystem.getLocal(conf);
+		fs.delete(output, true);
+		
+		FileSplitterDriver driver = new FileSplitterDriver();
+		driver.setConf(conf);
+		
+		int exitCode = driver.run(new String[]{input.toString(), output.toString() });
+		assertThat(exitCode, is(0));
+		
+	}
+
+}
